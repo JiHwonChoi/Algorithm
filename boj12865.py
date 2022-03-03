aa23763321bcdace1b70fb70704f0a4d2dd7c206
JiHwonChoi <luxuryhwon@g.skku.edu>
2022-03-03 20:09:55 +0900

    0303 냅색 알고리즘 수정중

--- boj12865.py
+++ boj12865.py
@@ -3,40 +3,31 @@ from cgi import test
 
 maxWeight=7
 itemNum=4
-items=[]
+items=[
+    (6,13),(4,8),(3,6),(5,12)
+]
 
-itemNum , maxWeight = map(int, input().split(' '))
-for i in range(itemNum):
-    a,b= map(int, input().split(' '))
-    items.append((a,b))
+# itemNum , maxWeight = map(int, input().split(' '))
+# for i in range(itemNum):
+#     a,b= map(int, input().split(' '))
+#     items.append((a,b))
 
-#가방안에 있는 아이템 넣는지 아닌지 기록하는 배열
-# 배열 생성하기 (0,0,0,0)~(1,1,1,1)
-testCase =[]
-for i in range(2**itemNum):
-    testCase.append("{0:b}".format(i).zfill(4))
+dp = [[0]*(maxWeight+1)]*(itemNum+1)
+for i in range(itemNum+1):
+    for j in range(maxWeight+1):
+        
+        if(i and j):
+            if(j-items[i][0]>=0):
+                dp[i][j]= max(dp[i-1][j],(dp[i][j-items[i][0]]+items[i][1]))#여기가 문제 dp[j-items[i][0]] 인덱스가 나가버린다
+            else:
+                dp[i][j]=dp[i-1][j]
+        elif (i==0):
+            dp[i][j]= 0
+        elif (j==0):
+            dp[i][j]=dp[i-1][j]
+    print(dp)
 
-maxValue = 0 
-
-for i in range(len(testCase)): # 0000 - 0001 - 0010 -...-1111
-    bag = 0
-    value = 0
-    for j in range(4): #0-1-2-3
-        if int(testCase[i][j]):
-            if(bag+items[j][0]<=maxWeight):
-                bag += items[j][0]
-                value += items[j][1]
-            else: break
-        if (value > maxValue): #마지막까지 확인후 최대값 비교하기
-            maxValue=value
             
-    print(testCase[i], bag)
-
-print(maxValue)
             
-            #리밋이 넘어가는지 확인해서 안넘어가면
-                #가방에 넣구
-            #넘어가면 이번 j루프는 종료하고 다음 i 루프로 넘어감
-            #종료할때 이게 마지막 값이라면 맥스값과 바교한 다음에 넘어가야함 #가치 연산은 여기서 하면 됨
             
             

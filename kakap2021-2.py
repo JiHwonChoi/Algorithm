ce568316e6c8ef7e2ab784496d5cf1804768f9d7
JiHwonChoi <86228531+JiHwonChoi@users.noreply.github.com>
2022-04-14 23:01:03 +0900

    kakao2021 2번 문제

--- kakap2021-2.py
+++ kakap2021-2.py
@@ -4,11 +4,43 @@ direction=[(1,0),(-1,0),(0,1),(0,-1)]
 
 
 for room in places:
+    # print(room)
+    roomflag=0
     for rowidx, row in enumerate(room):
+        # print(rowidx,row)
         for seatidx, seat in enumerate(row):
-            if(seat=='O'):
+            # print(seatidx,seat)
+            if(seat=='O'): #O의 상하좌우로 있나 P가 두개이상 있나 확인하기
                 findp=[]
                 for (a,b) in direction:
                     if( 0<=rowidx+a<5 and 0<=seatidx+b<5):
                         findp.append(room[rowidx+a][seatidx+b])
+                pflag=0
+                for x in findp:
+                    if(x=='P'):
+                        pflag+=1
+                if(pflag>1):#P 가 두개이상 있으면
+                    # print('roomflag',rowidx,seatidx,findp)
+                    roomflag=1
+                    break
+            elif(seat=='P'):
+                findp=[]
+                for (a,b) in direction:
+                    if( 0<=rowidx+a<5 and 0<=seatidx+b<5):
+                        findp.append(room[rowidx+a][seatidx+b])
+                pflag=0
+                for x in findp:
+                    if(x=='P'):
+                        roomflag=1
+                        break
+        if(roomflag):
+            break
+    if(roomflag):
+        ans.append(0)
+    else:
+        ans.append(1)
+    
+print(ans)
+                    
+                        
                     
